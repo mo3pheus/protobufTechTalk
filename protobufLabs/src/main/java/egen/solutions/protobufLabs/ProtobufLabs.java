6e5228e3cbb4bacf9440a5f60bdb45c9c66f35f0
Sanket Korgaonkar <sanket.korgaonkar@gmail.com>
2017-04-14 15:47:43 -0500

    Ready for talk!

--- protobufLabs/src/main/java/egen/solutions/protobufLabs/ProtobufLabs.java
+++ protobufLabs/src/main/java/egen/solutions/protobufLabs/ProtobufLabs.java
@@ -3,12 +3,14 @@ package egen.solutions.protobufLabs;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+import egen.solutions.protocol.buffers.HackerStudentOuterClass.HackerStudent;
 import egen.solutions.protocol.buffers.StudentOuterClass.Student;
 import egen.solutions.protocol.buffers.StudentOuterClass.Student.Book;
 import egen.solutions.protocol.buffers.StudentOuterClass.Student.CheckOut;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 /**
  * Hello world!
@@ -16,8 +18,12 @@ import com.google.gson.GsonBuilder;
  */
 public class ProtobufLabs {
 	public static final String BREAKER = "__________________________________________________________";
+	
+	public static void main(String[] args){
+		System.out.println("Welcome to protoocol buffers");
+	}
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 
 		// lets build a student object
 		Student mo3pheus = Student.newBuilder().setName("Morpheus").setAddress("Operating Deck Nebuchadnezzar").build();
@@ -48,18 +54,45 @@ public class ProtobufLabs {
 				.putBooksDue("Hack the Matrix", System.currentTimeMillis() + TimeUnit.DAYS.toMillis(2)).build();
 		System.out.println(mo3pheus2.toString());
 		System.out.println(BREAKER);
-		
+
 		System.out.println("Lets look at a Json representation.");
 		String secretsOfMorpheus = getJSonFromProject(codeComplete);
 		System.out.println(secretsOfMorpheus);
-		
+
 		System.out.println(BREAKER);
 		System.out.println("Lets get it back from the string");
 		Book bookOfSecrets = getProjectFromJSONString(secretsOfMorpheus);
 		System.out.println(bookOfSecrets.toString());
+
+		// parsing
+		System.out.println(BREAKER);
+		System.out.println("Parsing back and forth");
+		byte[] morphByteArr1 = mo3pheus2.toByteArray();
+
+		try {
+			Student morph34 = Student.parseFrom(morphByteArr1);
+			System.out.println(morph34.toString());
+		} catch (InvalidProtocolBufferException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// parsing
+		System.out.println(BREAKER);
+		System.out.println("But protobufs aren't secure");
+		byte[] morphByteArr12 = mo3pheus2.toByteArray();
+
+		try {
+			HackerStudent morph34 = HackerStudent.parseFrom(morphByteArr12);
+			System.out.println(morph34.toString());
+		} catch (InvalidProtocolBufferException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
-	public static Book getProjectFromJSONString(String jsonString ) {
+	public static Book getProjectFromJSONString(String jsonString) {
 		Book localProject = null;
 		try {
 			Gson gson = new Gson();
